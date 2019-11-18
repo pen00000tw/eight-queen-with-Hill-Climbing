60f5345a9879566052db59e122cdad70f6e2da6a
pen00000tw <38594943+pen00000tw@users.noreply.github.com>
2019-11-18 18:05:00 +0800

    first upload

---
+++ 八皇后/八皇后/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace 八皇后
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置 Managed 資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器
+        /// 修改這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("新細明體", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.textBox1.Location = new System.Drawing.Point(44, 50);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox1.Size = new System.Drawing.Size(572, 432);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("新細明體", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.button1.Location = new System.Drawing.Point(310, 579);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(106, 512);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 2;
+            this.textBox2.Text = "8";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(59, 515);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 12);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "皇后數";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(646, 50);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBox3.Size = new System.Drawing.Size(226, 432);
+            this.textBox3.TabIndex = 4;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(898, 699);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ 八皇后/八皇后/Form1.cs
@@ -0,0 +1,150 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace 八皇后
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+
+        }
+        int n = 0;
+        private void print(int []arr) //顯示到textbox
+        {
+            textBox1.Text = "";
+            //textBox1.Text = textBox1.Text + "--------------------------------------\r\n";
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (arr[i] == j)
+                    {
+                        textBox1.Text = textBox1.Text + "♞ ";
+                    }
+                    else
+                    {
+                        textBox1.Text = textBox1.Text + "◯ ";
+                    }
+                }
+                textBox1.Text = textBox1.Text + "\r\n";
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e) //main
+        {
+            textBox3.Text = "";
+            n = int.Parse(textBox2.Text);
+            int[] arr = new int[n];
+            int maxstep = 50;
+            int nowstep = 0;
+            int restart = 0;
+            while (conflict(arr) > 0)
+            {
+               
+                init(arr);
+                //print(arr);
+                textBox3.AppendText("restart times:" + restart + "\r\n");
+                textBox3.AppendText("conflict amount:" + conflict(arr) + "\r\n");
+                do
+                {
+                    HC(arr);
+                    nowstep++;
+                    textBox3.AppendText("try: " + nowstep + " conflict amount:" + conflict(arr) + "\r\n");
+                    Application.DoEvents();
+
+                } while (conflict(arr) > 0 && nowstep < maxstep); //如果嘗試超過maxstep的話重新開始
+                
+                restart++;
+                nowstep = 0;
+            }
+            print(arr);
+        }
+        private void init(int []arr)// 初始化
+        {
+            for (int i = 0; i < n; i++)
+            {
+                Random rnd1 = new Random(Guid.NewGuid().GetHashCode());
+                arr[i] = rnd1.Next(0, n);
+            }
+        }
+        private int conflict(int[] arr)
+        {
+            int num = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    if (arr[i] == arr[j])   //檢查直的
+                    {
+                        num++;
+                    }
+                    if (Math.Abs(arr[i] - arr[j]) == (j - i)) //檢查斜的
+                    {
+                        num++;
+                    }
+                }
+            }
+            return num;
+        }
+        private void HC(int[] arr)
+        {
+            int[] arr_copy = new int[n];
+            int conflictnow = 0;
+            int[,] tmp = new int[n, n];
+            int x = 0,y = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (arr[i] == j)
+                    {
+                        continue;
+                    }
+                    arr_copy = (int[])arr.Clone();
+                    arr_copy[i] = j;
+                    tmp[i,j] = conflict(arr_copy);
+                }
+            }
+            conflictnow = conflict(arr);
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    if (arr[i] == j)
+                    {
+                        continue;
+                    }
+                    else 
+                    {
+                        if (tmp[i, j] <= conflictnow)
+                        {
+                            conflictnow = tmp[i, j];
+                            x = i;
+                            y = j;
+                        }
+                    }
+                }
+            }
+            arr[x] = y;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
